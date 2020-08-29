f4aab9376e0f9fd557738227716a87d92dd2a8fc
Kimore <trusswork97@gmail.com>
2020-08-29 03:05:08 +0300

    fixed the issues

--- main.js
+++ main.js
@@ -1,39 +1,45 @@
 let navbar = $('.navbar');
 
-$(window).scroll(function(){
-    let oTop = $('.section-2').offset().top - window.innerHeight;
-    if($(window).scrollTop()> oTop){
-        navbar.addClass("sticky")
-    }else{
-        navbar.removeClass("sticky")
-    }
+$(window).scroll(function() {
+  let oTop = $('.section-2').offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky")
+  } else {
+    navbar.removeClass("sticky")
+  }
 
 
 });
 
-// counter
+// // counter
+// function counter() {
+//   $('.count').each(function() {
+//     $(this).prop('Counter', 0).animate({
+//       Counter: $(this).text()
+//     }, {
+//       duration: 4000,
+//       easing: 'swing',
+//       step: function(now) {
+//         $(this).text(Math.ceil(now));
+//       }
+//     });
+//   });
+// }
 
-// let nCount = function(selector){
-//     $(selector).each(function(){
-//         $(this).animate({
-//             Counter:$(this).text()
-//         },{
-//             duration:4000,
-//             easing:"swing",
-//             step:function(value){
-//                 $(this).text(Math.cell(value));
-//             }
 
-//         }
-//         )
-//     })
-// };
-// let a = 0;
-// $(window).scroll(function(){
-//     let oTop = $('.numbers').offset().top - window.innerHeight;
-//     if(a== 0 && $(window).scrollTop()>= oTop){
-//         a++;
-//         nCount('.rect>h1');
-//     }
+// $(window).scroll(function() {
+//   var numbersDivHeight = $("#content").offset().top;
 
+//   // you can reduce the offsetWidth to start the animation sooner or later than it is now
+//   var offsetWidth = 200;
+//   var scroll = $(window).scrollTop();
+//   console.log(Math.trunc(numbersDivHeight));
+//   console.log(scroll);
+//   console.log(scroll == Math.trunc(numbersDivHeight));
+
+//   if (scroll >= Math.trunc(numbersDivHeight) - offsetWidth) {
+//     counter();
+//     //assign an empty function to call COUNTER once , couldn't see a better option hehe
+//     counter = function(){};
+//   }
 // });
